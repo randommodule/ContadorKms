369f5fff198dbf58dc1540ac50dc50d3de1951df
byRandom <random.module@gmail.com>
2023-11-22 03:50:14 -0300

    Se ha añadido Hilos y otras cosas
    Se ha añadido Hilos, y se ha mejorado bastante todo

--- app/src/main/java/com/example/contadorkms/MainActivity.java
+++ app/src/main/java/com/example/contadorkms/MainActivity.java
@@ -1,156 +1,42 @@
 package com.example.contadorkms;
 
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationManager;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import com.google.android.gms.location.FusedLocationProviderClient;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
-
-    private Button startButton;
-    private Button stopButton;
-    private TextView distanceTextView;
-    private TextView speedTextView;
-
-    private Location actualLocation;
-    private LocationManager locationManager;
-    private boolean tracking = false;
-    private double totalDistance = 0;
-    private Location lastLocation;
-    private Handler handler;
+    private Button start;
+    private Button exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start=findViewById(R.id.startButton);
+        exit=findViewById(R.id.exitButton);
 
-        startButton = findViewById(R.id.startButton);
-        stopButton = findViewById(R.id.stopButton);
-        distanceTextView = findViewById(R.id.distanceTextView);
-        speedTextView = findViewById(R.id.speedTextView);
 
-        handler = new Handler(Looper.getMainLooper());
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    LOCATION_PERMISSION_REQUEST_CODE);
-        }
+
     }
-
-    public void startTracking(View view) {
-        tracking = true;
-        startButton.setEnabled(false);
-        stopButton.setEnabled(true);
-        totalDistance = 0;
-        lastLocation = null;
-
-        // Inicia el hilo para la obtención de la ubicación
-        startLocationUpdates();
+    public void closeApplication(View view) {
+        finishAffinity();
     }
-
-    public void stopTracking(View view) {
-        tracking = false;
-        startButton.setEnabled(true);
-        stopButton.setEnabled(false);
-        stopLocationUpdates();
+    public void openSecondActivity(View view) {
+        Intent intent = new Intent(this, PrincipalMenu.class);
+        startActivity(intent);
     }
-
-    private void startLocationUpdates() {
-        // Implementa tu lógica para obtener la ubicación aquí
-        // Puedes usar FusedLocationProviderClient, LocationManager, etc.
-        locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
-        // Este es solo un ejemplo simulado
-        Runnable locationRunnable = new Runnable() {
-            @Override
-            public void run() {
-                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
-                actualLocation = new Location(locationManager.getLastKnownLocation(Manifest.permission.ACCESS_FINE_LOCATION));
-                // Simulación de una nueva ubicación cada segundo
-                double latitude =  lastLocation.getLatitude();
-                        //Math.random() * 0.01 + 37.7749;
-                double longitude = lastLocation.getLongitude();
-                        //Math.random() * 0.01 - 122.4194;
-
-                Location newLocation = new Location("SimulatedProvider");
-                newLocation.setLatitude(latitude);
-                newLocation.setLongitude(longitude);
-
-                updateDistanceAndSpeed(newLocation);
-
-                if (tracking) {
-                    handler.postDelayed(this, 1000); // 1000 milisegundos = 1 segundo
-                }
-            }
-        };
-
-        handler.post(locationRunnable);
-    }
-
-    private void stopLocationUpdates() {
-        handler.removeCallbacksAndMessages(null);
-    }
-
-    private void updateDistanceAndSpeed(Location newLocation) {
-        if (lastLocation != null) {
-            float distance = lastLocation.distanceTo(newLocation);
-            totalDistance += distance;
-
-            double speed = distance / 1.0; // Cambia el divisor según tus necesidades para obtener la velocidad en m/s, km/h, etc.
-
-            final String distanceText = String.format("Distancia recorrida: %.2f km", totalDistance / 1000);
-            final String speedText = String.format("Velocidad: %.2f m/s", speed);
-
-            // Actualiza la interfaz de usuario en el hilo principal
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    distanceTextView.setText(distanceText);
-                    speedTextView.setText(speedText);
-                }
-            });
-        }
-
-        lastLocation = newLocation;
-    }
-
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permiso concedido, realiza las operaciones necesarias
-            } else {
-                // Permiso denegado, puedes mostrar un mensaje o realizar acciones adicionales
-            }
-        }
+    protected void onDestroy() {
+        super.onDestroy();
     }
-}
 
+
+
+}
 
---
+++ app/src/main/java/com/example/contadorkms/PrincipalMenu.java
@@ -0,0 +1,328 @@
+package com.example.contadorkms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+
+public class PrincipalMenu extends AppCompatActivity {
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    private Button startButton;
+    private Button stopButton;
+    private Button resetButton;
+    private TextView distanceTextView;
+    private TextView speedTextView;
+    private TextView calorias;
+
+    private LocationManager locationManager;
+    private boolean tracking = false;
+    private Spinner weightSpinner;
+    private double totalDistance = 0;
+    private Location lastLocation;
+    private boolean paused = false;
+    private boolean isFirstLocation = true;
+
+    private LocationHandlerThread locationHandlerThread;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal_menu);
+
+        startButton = findViewById(R.id.startButton);
+        stopButton = findViewById(R.id.stopButton);
+        resetButton = findViewById(R.id.resetButton);
+        distanceTextView = findViewById(R.id.distanceTextView);
+        speedTextView = findViewById(R.id.speedTextView);
+        calorias = findViewById(R.id.caloriesTextView);
+
+        weightSpinner=findViewById(R.id.weightSpinner);
+        String[] weightValues = new String[201];
+        for (int i = 0; i <= 200; i++) {
+            weightValues[i] = String.valueOf(i);
+        }
+
+        // Crea un adaptador y establece los valores al Spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, weightValues);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        weightSpinner.setAdapter(adapter);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetTracking(v);
+            }
+        });
+
+        locationManager = (LocationManager) getApplicationContext().getSystemService(LOCATION_SERVICE);
+
+        // Solicitar permisos
+        requestLocationPermission();
+
+        // Inicializa el LocationHandlerThread
+        locationHandlerThread = new LocationHandlerThread("LocationHandlerThread", locationListener);
+        locationHandlerThread.start();
+        locationHandlerThread.prepareHandler();
+        locationHandlerThread.setLocationListener(locationListener);
+    }
+
+    public void goToMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        } else {
+            startLocationUpdates();
+        }
+    }
+
+    public void startTracking(View view) {
+        if(weightSpinner.getSelectedItem().equals("0")){
+            Toast.makeText(this,"No ha seleccionado su peso",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            if (!tracking ) {
+                tracking = true;
+                startButton.setEnabled(false);
+                stopButton.setEnabled(true);
+
+                if (paused) {
+                    paused = false; // Se reinicia el indicador de pausa
+                    // No reiniciar totalDistance y lastLocation, para continuar desde donde se detuvo
+                } else {
+                    // Si no está pausado, se está iniciando desde cero, entonces reinicia los valores
+                    if (lastLocation == null) {
+                        // Solo reinicia si lastLocation es nulo, es decir, al inicio
+                        totalDistance = 0;
+                    }
+                }
+
+                locationHandlerThread.requestLocationUpdates(locationManager);
+            }
+        }
+
+
+        }
+
+
+    private Location getLastKnownLocation() {
+        return null;  // Implementa la lógica para obtener la última ubicación conocida
+    }
+
+    public void stopTracking(View view) {
+        if (tracking) {
+            tracking = false;
+            paused = false;
+            startButton.setEnabled(true);
+            stopButton.setEnabled(false);
+            stopLocationUpdates();
+        }
+    }
+
+    public void startLocationUpdates() {
+        if (locationHandlerThread != null) {
+            locationHandlerThread.requestLocationUpdates(locationManager);
+        } else {
+            // Manejar la situación donde locationHandlerThread es nulo
+            Log.e("PrincipalMenu", "locationHandlerThread is null");
+        }
+    }
+
+    private void stopLocationUpdates() {
+        if (!paused && locationManager != null) {
+            locationHandlerThread.stopLocationUpdates(locationManager);
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    speedTextView.setText("Velocidad: 0 m/s");
+                }
+            });
+        }
+    }
+
+    public void resetTracking(View view) {
+        totalDistance = 0;
+        lastLocation = null;
+        isFirstLocation = true;
+
+        distanceTextView.setText("Distancia recorrida: 0 km");
+        speedTextView.setText("Velocidad: 0 m/s");
+        calorias.setText("Calorias quemadas: 0 cal.");
+        paused=false;
+        startTracking(view);
+
+        Toast.makeText(this, "Recorrido reiniciado", Toast.LENGTH_SHORT).show();
+    }
+
+    private void updateDistanceAndSpeed(Location newLocation) {
+        if (lastLocation != null) {
+            float distance = lastLocation.distanceTo(newLocation);
+            totalDistance += distance;
+
+            long timeElapsed = (newLocation.getTime() - lastLocation.getTime()) / 1000;
+            if (timeElapsed > 0) {
+                double speed = distance / timeElapsed;
+
+                final String distanceText = String.format("Distancia recorrida: %.2f km", totalDistance / 1000);
+                final String speedText = String.format("Velocidad: %.2f m/s", speed);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        distanceTextView.setText(distanceText);
+                        speedTextView.setText(speedText);
+                        calorias.setText("Calorias quemadas: " + ((int)(1.03 * Integer.parseInt(weightSpinner.getSelectedItem().toString()) * (totalDistance / 1000))) + " cal.");
+                    }
+                });
+            }
+        }
+
+        lastLocation = newLocation;
+        if (isFirstLocation) {
+            lastLocation = newLocation;
+            isFirstLocation = false;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates();
+            } else {
+                // Permiso denegado, muestra un mensaje o realiza acciones adicionales
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopLocationUpdates();
+
+        if (locationHandlerThread != null) {
+            locationHandlerThread.quit();
+            locationHandlerThread = null;
+        }
+    }
+
+    private LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location newLocation) {
+            updateDistanceAndSpeed(newLocation);
+        }
+        // Otros métodos de LocationListener...
+    };
+
+
+
+
+    public class LocationHandlerThread extends HandlerThread {
+
+        private Handler handler;
+        private LocationListener locationListener;
+
+        public void setLocationListener(LocationListener listener) {
+            locationListener = listener;
+        }
+
+        public LocationHandlerThread(String name, LocationListener listener) {
+            super(name);
+            locationListener = listener;
+        }
+
+        public void postTask(Runnable task) {
+            handler.post(task);
+        }
+
+        public void prepareHandler() {
+            handler = new Handler(getLooper());
+        }
+
+        public void requestLocationUpdates(LocationManager locationManager) {
+            // Asegúrate de que el handler esté preparado antes de llamar a este método
+            if (handler == null) {
+                throw new IllegalStateException("Handler not prepared. Call prepareHandler() first.");
+            }
+
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    if (locationManager != null && locationListener != null) {
+                        if (ActivityCompat.checkSelfPermission(PrincipalMenu.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(PrincipalMenu.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            // TODO: Consider calling
+                            //    ActivityCompat#requestPermissions
+                            // here to request the missing permissions, and then overriding
+                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                            //                                          int[] grantResults)
+                            // to handle the case where the user grants the permission. See the documentation
+                            // for ActivityCompat#requestPermissions for more details.
+                            return;
+                        }
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, locationListener);
+                    }
+                }
+            });
+        }
+
+        public void stopLocationUpdates(LocationManager locationManager) {
+            // Asegúrate de que el handler esté preparado antes de llamar a este método
+            if (handler == null) {
+                throw new IllegalStateException("Handler not prepared. Call prepareHandler() first.");
+            }
+
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    if (locationManager != null && locationListener != null) {
+                        locationManager.removeUpdates(locationListener);
+                    }
+                }
+            });
+        }
+    }
+
+
+}
